47df258078fed2902acb9226b9057e3bae9d3e22
SemihYlmzz <ekkotaymmm@gmail.com>
2023-07-04 01:34:17 +0300

    # App icon adjustments.

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -8,16 +7,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'FUSION',
-            style: GoogleFonts.bangers(
-              fontSize: 150,
-            ),
-          ),
-        ),
-      ),
+      home: const Scaffold(),
     );
   }
 }
