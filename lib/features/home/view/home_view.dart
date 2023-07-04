6d4913e2ed87019c35121fcd60890268535a3950
SemihYlmzz <ekkotaymmm@gmail.com>
2023-07-04 16:44:55 +0300

    + Android Firebase linked. + Android google login added.

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,11 +1,22 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fusion/repositories/auth_repository/bloc/auth_bloc.dart';
+import 'package:shared_widgets/shared_widgets.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BaseColumn(
+      children: [
+        ElevatedButton(
+          child: const Text('Sign Out'),
+          onPressed: () {
+            context.read<AuthBloc>().add(const AuthLogoutRequested());
+          },
+        ),
+      ],
+    );
   }
 }
