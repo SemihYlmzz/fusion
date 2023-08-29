0367002586d252cdfb2565f6667ecd990395f955
isco2225 <omran.haj20@gmail.com>
2023-08-29 23:50:20 +0300

    +"Delete Accon" Screen Created.

--- lib/features/settings/widgets/pop_ups/delete_account_pop_up.dart
+++ lib/features/settings/widgets/pop_ups/delete_account_pop_up.dart
@@ -8,14 +8,25 @@ import 'package:shared_widgets/shared_widgets.dart';
 class DeleteAccountPopUp extends StatelessWidget {
   const DeleteAccountPopUp({required this.devicePrefs, super.key});
   final DevicePrefs devicePrefs;
+  static const _warningText = 'If you delete this account';
+  static const _deleteGradientButtonTitle = 'Delete';
+  static const _cancelGradientButtonTitle = 'Cancel';
+  static const String _text1 = 'The account is removed from your phone.';
+  static const String _text2 =
+      'In your accountyour edited decks of cards are deleted.';
+  static const String _text3 =
+      'Your match history and saved game settings will be reset';
+  static const String _text4 =
+      'You can continue to use the application by logging in to your account again within 30 days.';
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      contentPadding: SharedPaddings.all8,
+      backgroundColor: Colors.grey.shade900,
+      //contentPadding: SharedPaddings.all8,
       content: Container(
         constraints: const BoxConstraints(
-          maxHeight: 320,
+          maxHeight: 500,
         ),
         child: RawScrollbar(
           thumbVisibility: true,
@@ -24,34 +35,95 @@ class DeleteAccountPopUp extends StatelessWidget {
           radius: const Radius.circular(20),
           thickness: 5,
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                const Text('Are you shure to delete account ?'),
-                Row(
-                  children: [
-                    Expanded(
-                      child: GradientButton(
-                        text: 'YES',
-                        width: 100,
-                        onPressed: () {
-                          Navigator.popUntil(context, (route) => route.isFirst);
-                          // context.read<DeleteRequestBloc>().add(
-                          // const CreateDeleteRequestRequested(),
-                          // );
-                          context.goNamed(
-                            DeleteAccountScreen.name,
-                          );
-                        },
-                      ),
+            child: Padding(
+              padding: SharedPaddings.all8,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  const Padding(
+                    padding: SharedPaddings.bottom16,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Icon(
+                            Icons.warning,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Text(
+                            _warningText,
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                // Diğer dilleri benzer şekilde ekleyebilirsiniz.
-              ],
+                  ),
+                  Column(
+                    children: [
+                      _conditionsText(text: _text1),
+                      _conditionsText(text: _text2),
+                      _conditionsText(text: _text3),
+                      _conditionsText(text: _text4),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: GradientButton(
+                              text: _deleteGradientButtonTitle,
+                              width: 100,
+                              onPressed: () {
+                                Navigator.popUntil(
+                                    context, (route) => route.isFirst);
+                                // context.read<DeleteRequestBloc>().add(
+                                // const CreateDeleteRequestRequested(),
+                                // );
+                                context.goNamed(
+                                  DeleteAccountScreen.name,
+                                );
+                              },
+                            ),
+                          ),
+                          Expanded(
+                              child: GradientButton(
+                            text: _cancelGradientButtonTitle,
+                            width: 100,
+                            isOutlined: true,
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          )),
+                        ],
+                      ),
+                    ],
+                  ),
+                  // Diğer dilleri benzer şekilde ekleyebilirsiniz.
+                ],
+              ),
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _conditionsText({
+    required String text,
+  }) {
+    return Padding(
+      padding: SharedPaddings.bottom16,
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              '\u2022 $text',
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/shared/bottom_shared.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomShared extends StatelessWidget {
+  const BottomShared({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
