0a1a9b4af0b4fc2d62b795ef942be556d8f27831
SemihYlmzz <ekkotaymmm@gmail.com>
2023-10-12 01:07:16 +0300

    + copyWith method added to queue blocs state.

--- lib/blocs/queue/queue_state.dart
+++ lib/blocs/queue/queue_state.dart
@@ -8,6 +8,11 @@ abstract class QueueState extends Equatable {
   final String? errorMessage;
   final QueueModel? queue;
 
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  });
+
   @override
   List<Object?> get props => [
         errorMessage,
@@ -17,18 +22,47 @@ abstract class QueueState extends Equatable {
 
 class QueueEmpty extends QueueState {
   const QueueEmpty();
+
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  }) {
+    return const QueueEmpty();
+  }
 }
 
 class QueueLoading extends QueueState {
   const QueueLoading({super.queue});
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  }) {
+    return QueueLoading(queue: queue ?? this.queue);
+  }
 }
 
 class QueueReadyToEnter extends QueueState {
   const QueueReadyToEnter({super.queue});
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  }) {
+    return QueueReadyToEnter(queue: queue ?? this.queue);
+  }
 }
 
 class QueueHasData extends QueueState {
   const QueueHasData({required super.queue});
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  }) {
+    return QueueHasData(queue: queue ?? this.queue);
+  }
 }
 
 class QueueHasError extends QueueState {
@@ -40,8 +74,29 @@ class QueueHasError extends QueueState {
   });
   final ErrorCleanType errorCleanType;
   final ErrorDisplayType errorDisplayType;
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+    ErrorCleanType? errorCleanType,
+    ErrorDisplayType? errorDisplayType,
+  }) {
+    return QueueHasError(
+      queue: queue ?? this.queue,
+      errorMessage: errorMessage ?? this.errorMessage,
+      errorCleanType: errorCleanType ?? this.errorCleanType,
+      errorDisplayType: errorDisplayType ?? this.errorDisplayType,
+    );
+  }
 }
 
 class QueueLeaved extends QueueState {
   const QueueLeaved();
+  @override
+  QueueState copyWith({
+    String? errorMessage,
+    QueueModel? queue,
+  }) {
+    return const QueueLeaved();
+  }
 }
